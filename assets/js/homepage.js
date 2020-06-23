de663cae0602103430dbbce0da3c10913c7c3adf
Tom Breazier <tombreazier@macbook-pro.lan>
2020-06-22 21:34:48 -0500

    6.2.4 complete

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,18 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a Github username");
+    }
+}
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -10,4 +25,4 @@ var getUserRepos = function(user) {
     });
 };
 
-getUserRepos()
+userFormEl.addEventListener("submit", formSubmitHandler);
