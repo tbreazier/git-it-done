d49766935588ccce95fde7b6133f50dbdec07e1d
Tom Breazier <tombreazier@macbook-pro.lan>
2020-06-25 15:27:55 -0500

    displaying issues and error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,22 +1,38 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+getRepoName = function() {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if(repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    document.location.replace("./index.html");  
+  }
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
-        // request was successful
-        if (response.ok) {
-          response.json().then(function(data) {
-            displayIssues(data);
-          });
-        }
-        else {
-          alert("There was a problem with your request!");
-        }
-        if (response.headers.get("Link")) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayIssues(data);
+
+          // check if api has paginated issues
+          if (response.headers.get("Link")) {
             displayWarning(repo);
-            }
+          }
+        });
+      } else {
+        // if not successful, redirect to homepage
+        document.location.replace("./index.html");
+      }
     });
 }
 
@@ -69,4 +85,11 @@ var displayIssues = function(issues){
     }
 };
 
-  getRepoIssues("facebook/react");
+getRepoName = function() {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  getRepoIssues(repoName);
+  repoName.textContent = repoName;
+};
+
+getRepoName();
